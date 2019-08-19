2737143c80a781e43bc7bdef24332dfdd4814280
abbyshabi <abbyshabi@gmail.com>
2019-08-19 20:37:43 +0100

    deploying to heroku

--- folktale/api/views.py
+++ folktale/api/views.py
@@ -11,7 +11,7 @@ class CharacterListView(ListAPIView):
 
 class CharacterDetailView(RetrieveAPIView):
     queryset = Character.objects.all()
-    serializer_class = CharacterSerializern
+    serializer_class = CharacterSerializer
 
 class FolktaleListView(ListAPIView):
     queryset =Folktale.objects.all()
@@ -20,27 +20,3 @@ class FolktaleListView(ListAPIView):
 class FolktaleDetailView(RetrieveAPIView):
     queryset = Folktale.objects.all()
     serializer_class = FolktaleSerializer
-
-# class CharacterListView(APIView):
-#     def get(self,request,format = None):
-#         all_characterList = Character.objects.all()
-#         serializer_class = CharacterSerializer
-#         return Response(serializer_class.data)
-
-# class CharacterDetailView(APIView):
-#     def get(self,request,format = None):
-#         all_characterListDetail = Character.objects.all()
-#         serializer_class = CharacterSerializer
-#         return Response(serializer_class.data)
-
-# class FolktaleListView(APIView):
-#     def get(self,request,format = None):
-#         all_folktaleList = Folktale.objects.all()
-#         serializer_class = FolktaleSerializer
-#         return Response(serializer_class.data)
-
-# class FolktaleDetailView(APIView):
-#     def get(self,request,format = None):
-#         all_folktaleListDetail = Folktale.objects.all()
-#         serializer_class = FolktaleSerializer
-#         return Response(serializer_class.data)
